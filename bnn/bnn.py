fae59fe86b74131403771153e8f8b7f2e2c77d93
Ethan <e.goan@student.qut.edu.au>
2018-02-28 00:43:52 +1000

    changed tabs to spaces

--- bnn/bnn.py
+++ bnn/bnn.py
@@ -30,14 +30,14 @@ class DistNotImplementedError(NotImplementedError):
 
 class InvalidHyperparamsError(ValueError):
     def __init__(self, hyperparams):
-	print("Invalid keys in hyperparameters, keys supplied are:")
-	for key in hyperparams.keys():
-	    print(key)
-	print("Keys should be \"mean\" and \"var\" " )
+        print("Invalid keys in hyperparameters, keys supplied are:")
+        for key in hyperparams.keys():
+            print(key)
+        print("Keys should be \"mean\" and \"var\" " )
 
 class InvalidActivationError(ValueError):
     def __init__(self, activation):
-	print("Invalid activation name \"{}\" supplied")
+        print("Invalid activation name \"{}\" supplied")
     
 
 
@@ -54,18 +54,18 @@ def initialise_params(dims, dist = "normal", hyperparams = None):
     bias = a list of Edward model variables that hold the bias'
     dist = an optional input; a string that indicates the type of distribution we want
     hyperparams = an optional input; dict that will hold hyperparameters of how to initialise the 
-		  the weights/bias of each layer.
-		  Eg. for a normal distribution {'mean':0, 'var':1}
+                  the weights/bias of each layer.
+                  Eg. for a normal distribution {'mean':0, 'var':1}
 """
     #check all our inputs are correct format
     if(not isinstance(dims, list)):
-	raise DimensionTypeError(dims)
+        raise DimensionTypeError(dims)
     
     #now initialise the params as specified
     if(dist == "normal"):
-	weights, bias = _initialise_normal(dims, hyperparams)
+        weights, bias = _initialise_normal(dims, hyperparams)
     else:
-	raise DistributionNotImplementedError(dist)
+        raise DistributionNotImplementedError(dist)
     
     return weights, bias
     
@@ -77,30 +77,100 @@ def simple_feed_forward(x, weights, bias, activation):
     Description:
     Will forward pass input data through the layers
     Args:
-	x (tf.tensor) Input data as either an matrix or as a column vector
-	weights (tf.tensor)  list of weights for each layer
-	activation (list) list of strings indicating how activation is done at each layer
+        x (tf.tensor) Input data as either an matrix or as a column vector
+        weights (tf.tensor)  list of weights for each layer
+        activation (list) list of strings indicating how activation is done at each layer
     Returns:
-	(tf.tensor) output of network
+        (tf.tensor) output of network
     """
     for l in range(1, len(weights)):
-	#for the first pass will initialise output of the individual layer as the input data
-	if l = 0:
-	    A = X
-	Z = tf.matmul(weights[0], x) + bias[0]
-	#apply non-linear activation function
-	if(activation[l] == "tanh"):
-	    A = tf.tanh(Z)
-	elif(activation[l] == "relu"):
-	    A = tf.nn.relu(Z)
-	elif(activation[l] == "sigmoid"):
-	    A = tf.sigmoid(Z)
-	#if no activation is applied
-	elif(activation[l] == "none"):
-	    A = Z
-	#otherwise an invalid activation function has been supplied
-	else:
-	    raise InvalidActivationError(activation[l])
+        #for the first pass will initialise output of the individual layer as the input data
+        if l = 0:
+            A = X
+        Z = tf.matmul(weights[0], x) + bias[0]
+        #apply non-linear activation function
+        if(activation[l] == "tanh"):
+            A = tf.tanh(Z)
+        elif(activation[l] == "relu"):
+            A = tf.nn.relu(Z)
+        elif(activation[l] == "sigmoid"):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            A = tf.sigmoid(Z)
+        #if no activation is applied
+        elif(activation[l] == "none"):
+            A = Z
+        #otherwise an invalid activation function has been supplied
+        else:
+            raise InvalidActivationError(activation[l])
     return A
 
 
@@ -111,17 +181,17 @@ def _initialise_normal(dims, hyperparams):
     
     Description:
     Will in initialise parameters for Bayesian NN with Normal distribution
-    Keyword arguments:
-    dims = a list of ints that indicate the number of hidden units in each hidden layer
-    hyperparams = an optional input; dict that will hold hyperparameters of how to initialise the 
-		  the weights/bias of each layer.
-		  Eg. for a normal distribution {'mean':0, 'var':1}
+    Args:
+        dims = a list of ints that indicate the number of hidden units in each hidden layer
+        hyperparams = an optional input; dict that will hold hyperparameters of how to 
+        initialise the the weights/bias of each layer. Eg. for a normal
+        distribution {'mean':0, 'var':1}
     """
     #check that the hyperparameters are correct
     if(hyperparams == None):
-	hyperparams = {"mean":0, "var":1}
+        hyperparams = {"mean":0, "var":1}
     if('mean' not in hyperparams.keys()) or ('var' not in hyperparams.keys()):
-	raise InvalidHyperparamsError(hyperparams)
+        raise InvalidHyperparamsError(hyperparams)
 
     weights = []
     bias = []
@@ -129,8 +199,8 @@ def _initialise_normal(dims, hyperparams):
     var = np.float(hyperparams['var'])
     
     for ii in range(1, len(dims)):
-	weights.append(Normal(loc=tf.ones([dims[ii], dims[ii-1]]) * mean,
-			       scale=tf.ones([dims[ii], dims[ii-1]])*var))
-	bias.append(Normal(loc=tf.ones([dims[ii], dims[ii-1]]) * mean,
-			       scale=tf.ones([dims[ii], 1])*var))
+        weights.append(Normal(loc=tf.ones([dims[ii], dims[ii-1]]) * mean,
+                               scale=tf.ones([dims[ii], dims[ii-1]])*var))
+        bias.append(Normal(loc=tf.ones([dims[ii], dims[ii-1]]) * mean,
+                               scale=tf.ones([dims[ii], 1])*var))
     return weights, bias
